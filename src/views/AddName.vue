549b2fc265857933c4584013fe3724c6b3729d5d
Ayumi <ayumin@mac.local>
2018-08-11 23:17:23 +0900

    styleを追加

--- src/views/AddName.vue
+++ src/views/AddName.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="addName">
-    食材名を入力してください<br/>
-    <input v-model="food" type="text">
-    <button v-on:click="addFood()">追加</button>
+    Please input food name<br/>
+    <input v-model="food" type="text"><br/>
+    <button v-on:click="addFood()">Add</button>
   </div>
 </template>
 
@@ -18,7 +18,7 @@
     },
     methods: {
       addFood (event) {
-        alert(`食材は「${ this.food }」ですね`);
+        alert(`Food name is ${ this.food }.`);
         if (event) {
           alert(event.target.tagName);
         }
@@ -26,3 +26,37 @@
     }
   }
 </script>
+
+<style lang="scss" scoped>
+* {
+  font-family: Roboto;
+}
+div {
+  color: #616161;
+  font-size: 14px;
+  font-weight: bold;
+  bottom: 12px;
+}
+input {
+  margin-top: 10px;
+  padding: 0 0 4px;
+  border: none;
+  background: none;
+  border-bottom: 1px solid rgba(0,0,0,.12);
+}
+input:focus {
+    outline: 0;
+    border-color: #A5D6A7;
+}
+button {
+  outline: 0;
+  margin-top: 10px;
+  background-color: #A5D6A7;
+  color: #000000;
+  box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);
+}
+button:active{
+  background-color: #A5D6A7;
+}
+</style>
+
