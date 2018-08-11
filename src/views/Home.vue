7c14318757ca8e92c20b7bf5e0608a010850be93
Ayumi <ayumin@mac.local>
2018-08-11 20:25:03 +0900

    editorConfigを追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Example></Example>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Example from '@/components/Example.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    HelloWorld: HelloWorld,
+    Example: Example,
   },
 })
 export default class Home extends Vue {}
